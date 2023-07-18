5e1da92afc6f5e5bde832cedfd0db93e13e582bf
Rajnandani257 <138346174+Rajnandani257@users.noreply.github.com>
2023-07-18 07:56:48 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,8 +1,1 @@
-Hello Everyone, I'm Rajnandani Kumari
-I so glad to inform all that I got an
-opportunity to be as intern in OCTANET.
-Here I'm sharing my first task assigned to me
-which is A Landing page in which i used 
-HTML5 and CSS.
-Thank you so much for visiting 😊.
 
